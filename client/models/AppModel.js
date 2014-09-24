262891a398338c9fc21605daeec3a5b025d4ba4b
gilhooley <katherine.gilhooley@hackreactor.com>
2014-09-24 16:55:20 -0700

    Enqueue adds songs to songQueue

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -16,6 +16,20 @@ var AppModel = Backbone.Model.extend({
     params.library.on('play', function(song){
       this.set('currentSong', song);
     }, this);
-  }
+
+    params.library.on('enqueue', function(song){
+      this.get('songQueue').add(song);
+      console.log(this.attributes);
+    }, this);
+
+
+  },
+
+
+    // listen for enqueue
+    // take model that was queued/clicked
+    // push to songQueue
+    //
+
 
 });
