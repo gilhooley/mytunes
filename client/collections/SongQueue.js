333339474222b8d2b53ec367f8de163e3561ec76
gilhooley <k.gilhooley@gmail.com>
2015-02-28 16:41:14 -0800

    Feature- and spec-complete

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,26 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+
+    this.on('add', function() {
+      if(!this.at(1)) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(song) {
+      this.remove(song);
+    }, this);
+
+    this.on('ended', function() {
+      this.shift();
+      this.at(0) && this.playFirst();
+    }, this);
+
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
